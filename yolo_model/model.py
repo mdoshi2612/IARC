caa9af944143cc181441edd69bcf165766248b3a
Manav Doshi <200100094@iitb.ac.in>
2022-03-04 21:17:59 +0530

    Model.py changes

--- yolo_model/model.py
+++ yolo_model/model.py
@@ -1,7 +1,10 @@
 from tool.darknet2pytorch import Darknet
 from tool.torch_utils import *
 import cv2
-import pandas as pd
+import pandas as pd 
+
+
+
 
 def yolo():
     board=Darknet(config_file_path,inference=True)
@@ -9,21 +12,22 @@ def yolo():
     board.cuda()
     return board
 
-
 def my_detect(m,cv_img):
     use_cuda=True
     img=cv2.resize(cv_img, (m.width, m.height))
     img=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     boxes = do_detect(m, img, 0.3, 0.6, use_cuda)
     if len(boxes[0])==0:
-        return [False,0,0,0,0]
+        return [False,0,0,0,0,-1]
     box=boxes[0][0]
     h,w,c=cv_img.shape
     x1 = int(box[0] * w)
     y1 = int(box[1] * h)
     x2 = int(box[2] * w)
     y2 = int(box[3] * h)
-    return [True,x1,y1,x2,y2]
+    score = box[4]
+    return [True,x1,y1,x2,y2,score]
+
 
 def draw_box(image):
     ret, x1,y1,x2,y2,score = my_detect(board, image)
