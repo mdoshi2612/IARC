7f0c1ff06f2721a47b0836489d57dd84c2a693a0
Manav Doshi <200100094@iitb.ac.in>
2022-03-05 16:17:27 +0530

    Video displayed

--- yolo_model/yolo_sort.py
+++ yolo_model/yolo_sort.py
@@ -258,7 +258,6 @@ class Sort(object):
 def parse_args():
     """Parse input arguments."""
     parser = argparse.ArgumentParser(description='SORT demo')
-    parser.add_argument('--display', dest='display', help='Display online tracker output (slow) [False]',action='store_true')
     parser.add_argument("--seq_path", help="Path to detections.", type=str, default='data')
     parser.add_argument("--phase", help="Subdirectory in seq_path.", type=str, default='train')
     parser.add_argument("--max_age", 
@@ -274,14 +273,13 @@ def parse_args():
 if __name__ == '__main__':
   # all train
   args = parse_args()
-  display = args.display
   phase = args.phase
   total_time = 0.0
   total_frames = 0
   colours = np.random.rand(32, 3) #used only for display
 
   board = model.yolo(config_file_path = './custom-yolov4-detector.cfg', weight_file_path = './custom-yolov4-detector_4000.weights')
-  image = cv2.imread('test.png')
+
 
 # Displaying a basic test image
 
@@ -292,63 +290,18 @@ if __name__ == '__main__':
 #    if cv2.waitKey(0) == ord('q'):
 #      break
 
-if(display):
-  if not os.path.exists('mot_benchmark'):
-    print('\n\tERROR: mot_benchmark link not found!\n\n    Create a symbolic link to the MOT benchmark\n    (https://motchallenge.net/data/2D_MOT_2015/#download). E.g.:\n\n    $ ln -s /path/to/MOT2015_challenge/2DMOT2015 mot_benchmark\n\n')
-    exit()
-  plt.ion()
-  fig = plt.figure()
-  ax1 = fig.add_subplot(111, aspect='equal')
+  cap = cv2.VideoCapture("./Video and Image Extractor/vid.mp4")
 
-  if not os.path.exists('output'):
-    os.makedirs('output')
-  pattern = os.path.join(args.seq_path, phase, '*', 'det', 'det.txt')
-  for seq_dets_fn in glob.glob(pattern):
-    mot_tracker = Sort(max_age=args.max_age, 
-                       min_hits=args.min_hits,
-                       iou_threshold=args.iou_threshold) #create instance of the SORT tracker
-
-    seq_dets = np.loadtxt(seq_dets_fn, delimiter=',')
-    seq = seq_dets_fn[pattern.find('*'):].split(os.path.sep)[0]
-
-    with open(os.path.join('output', '%s.txt'%(seq)),'w') as out_file:
-      print("Processing %s."%(seq))
-      for frame in range(int(seq_dets[:,0].max())):
-        frame += 1 #detection and frame numbers begin at 1
-        if(seq_dets[seq_dets[:, 0]==frame, 10] == 0):
-          is_empty = True
-        else:
-          is_empty = False
-        dets = seq_dets[seq_dets[:, 0]==frame, 2:7]
-        dets[:, 2:4] += dets[:, 0:2] #convert to [x1,y1,w,h] to [x1,y1,x2,y2]
-        total_frames += 1
-
-        if(display):
-          fn = os.path.join('mot_benchmark', phase, seq, 'img1', '%06d.jpg'%(frame))
-          im =io.imread(fn)
-          ax1.imshow(im)
-          plt.title(seq + ' Tracked Targets')
-
-        start_time = time.time()
-        if is_empty:
-          dets = np.empty((0, 5))
-          print(f"Frame {frame} has empty detection")
-        trackers = mot_tracker.update(dets)
-        cycle_time = time.time() - start_time
-        total_time += cycle_time
-
-        for d in trackers:
-          print('%d,%d,%.2f,%.2f,%.2f,%.2f,1,-1,-1,-1'%(frame,d[4],d[0],d[1],d[2]-d[0],d[3]-d[1]),file=out_file)
-          if(display):
-            d = d.astype(np.int32)
-            ax1.add_patch(patches.Rectangle((d[0],d[1]),d[2]-d[0],d[3]-d[1],fill=False,lw=3,ec=colours[d[4]%32,:]))
-
-        if(display):
-          fig.canvas.flush_events()
-          plt.draw()
-          ax1.cla()
-
-  print("Total Tracking took: %.3f seconds for %d frames or %.1f FPS" % (total_time, total_frames, total_frames / total_time))
-
-  if(display):
-    print("Note: to get real runtime results run without the option: --display")
+  while True:
+    ret, frame = cap.read()
+    if ret:
+        # if video is still left 
+        cv2.imshow("Video", frame)
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+          break
+    else:
+        break
+  
+# Release all space and windows once done
+  cap.release()
+  cv2.destroyAllWindows()
